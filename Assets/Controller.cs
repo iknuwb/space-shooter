fbcda013667028ec38d7ed8f8a86edebd33ed209
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2017-02-19 19:45:45 +0100

    first commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject player;
+	public Rigidbody2D playerPhysics;
+	public Transform playerUp;
+
+	// Use this for initialization
+	void Start () {
+		//playerPhysics=player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			
+			playerPhysics.AddForce((playerUp.position-player.transform.position)*0.5f,ForceMode2D.Impulse);
+
+			playerPhysics.velocity = Vector3.ClampMagnitude(playerPhysics.velocity, 2f);
+
+
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+
+			//playerPhysics.AddForce(-playerUp.position,ForceMode2D.Impulse);
+			//if(playerPhysics.velocity.magnitude > 1.5f){
+			//	playerPhysics.velocity = Vector3.ClampMagnitude(playerPhysics.velocity, 1.5f);
+			//}
+
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			player.transform.Rotate(new Vector3(player.transform.rotation.x,player.transform.rotation.y,player.transform.rotation.z+ 5)*100*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			player.transform.Rotate(new Vector3(player.transform.rotation.x,player.transform.rotation.y,player.transform.rotation.z-5)*100*Time.deltaTime);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# space-shooter
+
+English
+-------
+Simple space shooter game from [Unity](https://unity3d.com/) workshops.
+
+[Graphics](http://kenney.nl/assets/space-shooter-extension) from [kenney.nl](http://kenney.nl/) on [CC0 1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/) license.
+
+
+Polski
+------
+Prosta kosmiczna strzelanka z warsztatów [Unity](https://unity3d.com/).
+
+[Grafiki](http://kenney.nl/assets/space-shooter-extension) pochodzą z [kenney.nl](http://kenney.nl/) i zostały udostępnione na licencji [CC0 1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/deed.pl).
