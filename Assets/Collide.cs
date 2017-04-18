86eb2c4eaed3a6647ea893d14f4a8de29b215838
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2017-04-18 17:00:58 +0200

    Source code from workshop 2017-04-06 with better Particle

--- Assets/Collide.cs
+++ Assets/Collide.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Collide : MonoBehaviour {
 
 	public int lives;
+	public GameObject[] liveGraph;
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.transform.tag == "Asteroid"){
@@ -12,6 +13,29 @@ public class Collide : MonoBehaviour {
 				lives--;
 				Debug.Log (lives);
 			}
+		}
+	}
+
+	void Update(){
+		if(lives == 3){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = true;
+		}
+		else if(lives == 2){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = false;
+		}
+		else if(lives == 1){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = true;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = false;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = false;
+		}
+		else if(lives == 0){
+			liveGraph [0].GetComponent<SpriteRenderer> ().enabled = false;
+			liveGraph [1].GetComponent<SpriteRenderer> ().enabled = false;
+			liveGraph [2].GetComponent<SpriteRenderer> ().enabled = false;
 		}
 	}
 
