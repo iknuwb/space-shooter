3d9a66a7b179cb757055c3e3997afd0da40728b7
Gareeb <72250606+Gareeb-coder@users.noreply.github.com>
2020-10-04 12:01:35 +0530

    Update Gameover.cs

--- Assets/Gameover.cs
+++ Assets/Gameover.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Gameover : MonoBehaviour
 {
-   public void restart(){SceneManager.LoadScene(0) ;}
-   public void quit(){Application.Quit() ;}
+   public void restart() {
+      SceneManager.LoadScene(0);
+   }
+   public void quit() { 
+      Application.Quit();
+   }
 }
